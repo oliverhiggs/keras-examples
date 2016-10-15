a1abd94ef09edde31f9139ac3109ce04170424e6
Oliver Higgs <oliver.s.higgs@gmail.com>
2016-10-15 15:57:41 +1000

    Tidy up circles example

--- circles.py
+++ circles.py
@@ -2,7 +2,6 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers import Merge
 from keras.optimizers import SGD
-from keras.utils.visualize_util import plot
 
 from vis_tools import progress_plot
 
@@ -11,7 +10,7 @@ import numpy as np
 
 def generate_circle_data(radii, numPoints, maxPerterbutation):
     ''' Create training cases for a circle configuration
-    Points are drawn randomly from circles of different radii with each being 
+    Points are drawn randomly from circles of different radii with each being
     assigned to a different class
     The points will be perturbed randomly from the circle from which they are
     drawn
@@ -35,32 +34,32 @@ def generate_circle_data(radii, numPoints, maxPerterbutation):
     return (np.array(X), np.array(Y))
 
 
+radii = [1, 1.5, 2, 2.5, 3, 3.5]
+numPoints = 200
+maxPerterbutation = 0.7
+
 # Create the neural network model
 model = Sequential([
     Dense(10, input_dim=2),
     Activation('sigmoid'),
     Dense(10),
     Activation('sigmoid'),
-    Dense(4),
+    Dense(len(radii)),
     Activation('softmax')])
 
 # Compile the model with an optimiser
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(optimizer=sgd, loss='binary_crossentropy')
 
-radii = [1, 2, 3, 4]
-numPoints = 100
-maxPerterbutation = 0.1
-
 X, Y = generate_circle_data(radii, numPoints, maxPerterbutation)
 
 # Create a map of points to plot the function stored in the neural network
-bottomLeft = [-5, -5]
-topRight = [5, 5]
+bottomLeft = [-(max(radii) + 1), -(max(radii) + 1)]
+topRight = [max(radii) + 1, max(radii) + 1]
 
 # Create a plot to visualise the progress of training
-epochList = [1, 10, 20, 50, 100, 200, 300, 400, 500]
+epochList = [1, 10, 20, 50, 100, 300, 400, 500, 2000]
 batchSize = 10
 
 progress_plot(model, X, Y, bottomLeft, topRight, epochList, batchSize,
-              verbose=True)
+              verbose=False)
